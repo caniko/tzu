14e3fed37b1466aac9f4d22f9b6b55ae420fdf19
Can H. Tartanoglu <gpg@rotas.mozmail.com>
2026-08-14 18:26:34 +0200

    chore: refresh flake inputs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Local-first coding project planner backed by ACP agents";
 
   inputs = {
-    rs-harbor.url = "git+https://codeberg.org/caniko/rs-harbor.git?ref=trunk&rev=c26b735eede8078f795651c4a9cbf0be8733b221";
+    rs-harbor.url = "git+ssh://git@codeberg.org/caniko/rs-harbor.git?ref=trunk&rev=f209ddbca3fdbb0dc31fa3886ccc2ff7369c18ac";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
     crane.url = "github:ipetkov/crane";
