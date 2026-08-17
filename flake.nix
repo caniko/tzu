212f3a1f232bacf1b077b01b5b65849b2824beb1
Can H. Tartanoglu <gpg@rotas.mozmail.com>
2026-08-17 11:11:23 +0200

    chore: bump rs-harbor pin to 77d0a93 (dedup overlapping cross targets)

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Local-first coding project planner backed by ACP agents";
 
   inputs = {
-    rs-harbor.url = "git+ssh://git@codeberg.org/caniko/rs-harbor.git?ref=trunk&rev=f209ddbca3fdbb0dc31fa3886ccc2ff7369c18ac";
+    rs-harbor.url = "git+https://github.com/caniko/rs-harbor.git?ref=trunk&rev=77d0a937c760e6ced8b7ec8fc5a214f550abe35e";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
     crane.url = "github:ipetkov/crane";
